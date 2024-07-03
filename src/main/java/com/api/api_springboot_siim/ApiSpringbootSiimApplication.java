dc5fc129cefb965bcee3d9555458849e1cf68391
arielGs_Dev <arielogavilanes1534@gmail.com>
2024-07-03 09:28:21 -0500

    first commit

---
+++ src/main/java/com/api/api_springboot_siim/ApiSpringbootSiimApplication.java
@@ -0,0 +1,13 @@
+package com.api.api_springboot_siim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringbootSiimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringbootSiimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/api_springboot_siim/ApiSpringbootSiimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.api_springboot_siim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringbootSiimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
